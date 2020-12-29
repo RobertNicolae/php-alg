aef34e9145e5232633b6ed423e32ba80bdfd1cb3
Robert Balanoiu <balanoiurobert19@gmail.com>
2020-12-29 18:57:18 +0200

    updateWordP

--- wordPattern.php
+++ wordPattern.php
@@ -1,17 +1,41 @@
 <?php
-$pattern = 'abba';
-$string =  "dog fish fish dog";
-$string2 = "sd";
-$patternArr = [];
-function checkPattern(string $pattern, string $string): bool{
-    for ($i = 0; $i < strlen($pattern); $i++){
+
+
+
+function checkPattern(string $pattern, string $string)
+{
+    $patternArr = [];
+    $sum1 = 0;
+    $sum2 = 0;
+    for ($i = 0; $i < strlen($pattern); $i++) {
         $patternArr[] = $pattern[$i];
     }
-    if()
+    for ($i = 0; $i < count($patternArr) - 1; $i++) {
+        if ($patternArr[$i] === $patternArr[$i + 1]) {
+            $sum1 += 1;
+        } else {
+            $sum1 += 2;
+        }
+    }
+    $stringArr = explode(" ", $string);
+    for ($i = 0; $i < count($stringArr) - 1; $i++) {
+        if ($stringArr[$i] === $stringArr[$i + 1]) {
+            $sum2 += 1;
+        } else {
+            $sum2 += 2;
+        }
+    }
+    if ($sum1 === $sum2) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
+echo checkPattern("bbba", "cat cat cat dog");
 
 
 
-var_dump($arr);
+
 
