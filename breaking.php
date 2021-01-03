8bc02bef7250c10bc63c4987f39f1a0014beddb8
Robert Balanoiu <balanoiurobert19@gmail.com>
2021-01-03 13:01:31 +0200

    update breakingRecords

--- breaking.php
+++ breaking.php
@@ -2,20 +2,17 @@
 $scores = [10, 5, 20, 20, 4, 5, 2, 25, 1];
 $countMin = 0;
 $countMax = 0;
-$firstMatch = $scores[0];
-$lastBreak = $scores[0];
-$lastLow = $scores[1];
+$maxRecord = $scores[0];
+$minRecord = $scores[0];
 
 
-
-for($i = 0; $i < count($scores); $i++){
-    if($scores[$i] < $firstMatch && $scores[$i] !== $lastLow){
-        $countMin++;
-        $lastLow = $scores[$i];
-    } else if ($scores[$i] > $firstMatch && $scores[$i] > $lastBreak){
+foreach ($scores as $score) {
+    if ($score > $maxRecord){
+        $maxRecord = $score;
         $countMax++;
-        $lastBreak = $scores[$i];
+    } else if($score < $minRecord){
+        $minRecord = $score;
+        $countMin++;
     }
-
 }
 var_dump([$countMax, $countMin]);
